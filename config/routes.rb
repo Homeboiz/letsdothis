6667e48b374ab1e8c162c907e81fb1974ca87c85
Fabian Wahlers <fabianwahlers@hotmail.de>
2015-11-09 23:57:06 +0100

    added final changes to excercise 35

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
 
+  devise_for :users
+  resources :users
   resources :products
   match 'static_pages/about', via: [:get], to: 'static_pages#about',
   as: 'about'
